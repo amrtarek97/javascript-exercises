7709bac552ccf5043a9d0e8e48439343d1396a36
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-04-01 14:02:12 +0200

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number ==0) return 'error';
+    let str ='';
+    for (let i=0; i<number;i++){
+        str+=word;
+    }
+    return str;
 };
 
 // Do not edit below this line
