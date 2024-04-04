9d76bd400e0d296ffffa7e3c6316b795d4a561d4
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-04-04 21:38:47 +0200

    re solve remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...exception) {
+    // return array.filter((val) => !exception.includes(val)); Sol 1
+    // sol 2 using for each
+    const newArr = [];
+
+    array.forEach(element => {
+        if (!exception.includes(element)){
+            newArr.push(element);
+        }
+    });
+    return newArr;
 
 };
 
