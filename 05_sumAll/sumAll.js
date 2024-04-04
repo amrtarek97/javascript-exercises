5e2d51444e297814c95cfb0fad5ec82cf18b9f86
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-04-04 21:48:16 +0200

    re solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(no1, no2) {
+    let  total = 0;
+    if (!Number.isInteger(no1) || !Number.isInteger(no2)) return "ERROR";
+
+    if (no1 <0 || no2 <0 ){
+        return "ERROR";
+    } else if (no1<no2){
+        for (let i = no1; i <= no2; i++) {
+            total +=i;
+            
+        }
+    } else {
+        for (let i = no2; i <= no1; i++) {
+            total +=i;
+            
+        }
+    }
+    
+    return total;
 
 };
 
